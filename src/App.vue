95a62948c4506292be5ab214fcc81d4e0a138925
Yhiazer <b.urrea01@ufromail.cl>
2024-05-17 16:26:51 -0400

    tutorial 2

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <ComponentePadre />
+  </div>
+</template>
+
+<script>
+import ComponentePadre from './components/ComponentePadre.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ComponentePadre
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+color: #3498db;
+}
+/* más esti*/
+</style>
